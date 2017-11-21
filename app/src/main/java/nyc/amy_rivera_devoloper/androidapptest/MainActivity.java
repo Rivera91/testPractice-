b5f2ed9c146c17d137fe23d1937aecce7133bde5
Amy Rivera <amyrivera.ar@gmail.com>
2017-11-20 19:54:27 -0500

    first commit

---
+++ app/src/main/java/nyc/amy_rivera_devoloper/androidapptest/MainActivity.java
@@ -0,0 +1,13 @@
+package nyc.amy_rivera_devoloper.androidapptest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
